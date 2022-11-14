cb8f2906d3df8dffbf4e854ca4323473d3308728
fernando <crosio.fernando@gmail.com>
2022-11-13 22:41:52 -0300

    4to commit

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.listen(PORT, ()=>{
 })
 
 const dbOption= {
-    host: process.env.DB_HOST, 
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: DB_HOST, 
+    port: DB_PORT,
+    user: DB_USER, 
+    password: DB_PASSWORD,
+    database: DB_DATABASE
 
 
 }
